4024a4b44cb5f8f513b46591cd774d2f4afea8c6
RasmusTufvesson <lars@bizzbook.com>
2023-05-02 22:23:22 +0200

    update styling

--- src/app.rs
+++ src/app.rs
@@ -34,34 +34,65 @@ impl eframe::App for App {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading(&self.window_name);
-            if ui.button("Paste").clicked() {
-                let result: SysResult<String> = get_clipboard(formats::Unicode);
-                match result {
-                    Ok(string) => {
-                        for line in string.split("\n") {
-                            if line != "" {
-                                self.paste_lines.push(line.to_string());
-                            } 
-                        }
-                    },
-                    _ => {}
+            ui.with_layout(egui::Layout { main_dir: egui::Direction::TopDown, main_wrap: false, main_align: eframe::emath::Align::Min, main_justify: false, cross_align: eframe::emath::Align::Center, cross_justify: true }, |ui: &mut egui::Ui| {
+                ui.heading(&self.window_name);
+                ui.label(self.paste_lines.len().to_string() + " messages");
+                if ui.button("Paste").clicked() {
+                    let result: SysResult<String> = get_clipboard(formats::Unicode);
+                    match result {
+                        Ok(string) => {
+                            for line in string.split("\n") {
+                                let string = line.to_string();
+                                if line != "" && !self.paste_lines.contains(&string) {
+                                    self.paste_lines.push(string);
+                                } 
+                            }
+                        },
+                        _ => {}
+                    }
                 }
-            }
-            if ui.button("Send").clicked() {
-                for chunk in self.paste_lines.chunks(5) {
-                    let chuck_string = chunk.join("\n");
-                    self.to_send.blocking_send(chuck_string).unwrap();
+                if ui.button("Send").clicked() {
+                    for chunk in self.paste_lines.chunks(5) {
+                        let chuck_string = chunk.join("\n");
+                        self.to_send.blocking_send(chuck_string).unwrap();
+                    }
+                    self.paste_lines.clear();
                 }
-            }
+            });
         });
     }
 }
 
 pub fn main(bot_name: String, to_send: Sender<String>) -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        always_on_top: true,
+        maximized: false,
+        decorated: true,
+        fullscreen: false,
+        drag_and_drop_support: false,
+        icon_data: None,
+        initial_window_pos: None,
+        initial_window_size: Some(egui::vec2(300.0,100.0)),
+        min_window_size: None,
+        max_window_size: None,
+        resizable: false,
+        transparent: false,
+        mouse_passthrough: false,
+        vsync: true,
+        multisampling: 0,
+        depth_buffer: 0,
+        stencil_buffer: 0,
+        hardware_acceleration: eframe::HardwareAcceleration::Preferred,
+        renderer: eframe::Renderer::Glow,
+        follow_system_theme: false,
+        default_theme: eframe::Theme::Dark,
+        run_and_return: true,
+        event_loop_builder: None,
+        shader_version: None,
+        centered: true
+    };
     eframe::run_native(
-        "eframe template",
+        &(bot_name.clone() + " control"),
         native_options,
         Box::new(|cc| Box::new(App::new(cc, bot_name, to_send))),
     )
